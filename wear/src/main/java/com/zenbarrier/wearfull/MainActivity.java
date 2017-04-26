41466c36b655be802615ba80e29d6005e5aabee6
ZenBarrier <anthony.j.barrera@gmail.com>
2017-04-26 16:33:44 -0400

    Display version name in wear activity.

--- wear/src/main/java/com/zenbarrier/wearfull/MainActivity.java
+++ wear/src/main/java/com/zenbarrier/wearfull/MainActivity.java
@@ -1,25 +1,33 @@
 package com.zenbarrier.wearfull;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends Activity {
 
-    private TextView mTextView;
-
+    @SuppressLint("StringFormatInvalid")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
+
+        TextView textMain = (TextView) findViewById(R.id.textView_main);
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            String version = packageInfo.versionName;
+            textMain.setText(getString(R.string.hello_wear, version));
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            textMain.setText(getString(R.string.hello_wear, "2.x"));
+        }
+
     }
 }
